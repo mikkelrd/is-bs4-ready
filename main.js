11680f7eee09e92df0e67be1b3e2e85a308c1683
Mikkel Davis <mdavis@izeni.com>
2016-09-28 15:33:31 -0600

    Complete basic display

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
-var request = new XMLHttpRequest();
-request.addEventListener("load", function(){
-  var response = JSON.parse(this.responseText);
-  console.log(response);
+document.addEventListener('DOMContentLoaded', function() {
 
-  
+  var request = new XMLHttpRequest();
+
+  request.addEventListener('load', function(){
+    console.log(JSON.parse(this.responseText));
+    var miles = document.getElementById('miles');
+
+    JSON.parse(this.responseText).forEach(function(mile){
+      var newMile = document.createElement('progress');
+
+      newMile.classList.add('progress');
+      newMile.setAttribute('value', Math.ceil(mile.open_issues / (mile.open_issues + mile.closed_issues) * 100));
+      newMile.setAttribute('max', 100);
+
+      miles.appendChild(newMile);
+    });
+
+  });
+
+  request.open('GET', 'https://api.github.com/repos/twbs/bootstrap/milestones');
+  request.send();
+
 });
-request.open("GET", "https://api.github.com/repos/twbs/bootstrap/milestones");
-request.send();
