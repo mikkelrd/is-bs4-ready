2d2eb890915e0bedde15ae2522532e4342e5db4a
Mikkel Davis <mdavis@izeni.com>
2016-09-28 17:12:59 -0600

    Add basic styling and layout

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Is Bootstrap 4.0 ready to use?
+# Is Bootstrap 4 ready to use?
 A page for displaying the development progress of the long-awaited Bootstrap version 4.0. https://mikkelrd.github.io/is-bs4-ready
--- main.js
+++ main.js
@@ -1,19 +1,31 @@
 document.addEventListener('DOMContentLoaded', function() {
+  var milesContainer = document.getElementById('miles');
+  var milesTemplate = milesContainer.firstElementChild.cloneNode(true);
+  while (milesContainer.firstChild) {
+    milesContainer.removeChild(milesContainer.firstChild);
+  }
 
   var request = new XMLHttpRequest();
 
   request.addEventListener('load', function(){
     console.log(JSON.parse(this.responseText));
-    var miles = document.getElementById('miles');
 
-    JSON.parse(this.responseText).forEach(function(mile){
-      var newMile = document.createElement('progress');
+    JSON.parse(this.responseText)
+    .filter(function(mile){ return mile.title.includes('v4'); })
+    .sort(function(prevMile, currMile){ return prevMile.number > currMile.number })
+    .forEach(function(mile){
+      var newMile = milesTemplate.cloneNode(true);
+      var label = newMile.children[0];
+      var progress = newMile.children[1];
 
-      newMile.classList.add('progress');
-      newMile.setAttribute('value', Math.ceil(mile.open_issues / (mile.open_issues + mile.closed_issues) * 100));
-      newMile.setAttribute('max', 100);
+      label.children[0].textContent = mile.title;
+      label.children[0].setAttribute('href', 'https://github.com/twbs/bootstrap/milestone/' + mile.number);
+      label.children[1].textContent = 'Closed: ' + mile.closed_issues + ' ' + 'Open: ' + mile.open_issues;
 
-      miles.appendChild(newMile);
+      progress.setAttribute('value', mile.closed_issues);
+      progress.setAttribute('max', mile.open_issues + mile.closed_issues);
+
+      milesContainer.appendChild(newMile);
     });
 
   });
